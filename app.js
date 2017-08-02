a80a25f4228fccdc76a809eb2400f0c547baef03
Tejas Pancholi <tejas.er@outlook.com>
2017-08-02 13:09:41 +0530

    added API call to get temprature

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const axios = require('axios');
+const config = require('./config');
+
+var secretkey = config.SECRET_KEY;
 
 const port = process.env.PORT || 8888;
 const index = require('./routes/index');
@@ -13,4 +16,27 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
-const getApiAndEmit = "TODO";
+
+let interval;
+
+io.on('connection', socket => {
+    console.log('New client');
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 10000);
+    socket.on('disconnect', () => {
+        console.log('client disconnected.')
+    })
+})
+
+const getApiAndEmit = async socket => {
+    try {
+        const res = await axios.get('https://api.darksky.net/forecast/'+secretkey+'/17.3850,78.4867');
+        socket.emit('FromAPI', res.data.currently.temprature);
+    } catch (error) {
+        console.log(`Error: ${error.code}`);
+    }
+}
+
+server.listen(port, () => console.log(`Listening on Port: ${port}`))
